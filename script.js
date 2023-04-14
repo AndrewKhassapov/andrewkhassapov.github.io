5ee44af668b649775c7fab3a041c8f08058617bd
AndrewKhassapov <Andrew.Khassapov@gmail.com>
2023-04-15 03:42:21 +1000

    GitHub Pages does not support node!
    This is pointless.
    GitHub pages does not support node.js and npm, natively.
    It is for static web pages.
    I will need to either run a node.js server or scale back the OrbitControls.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 import * as THREE from "/node_modules/three/build/three.module.js";
-import { OrbitControls } from "/node_modules/three-orbitcontrols/OrbitControls.js";
-
+//import { OrbitControls } from "/node_modules/three-orbitcontrols/OrbitControls.js";
+import { OrbitControls } from "/node_modules/three/examples/jsm/controls/OrbitControls.js"
 
 let scene,
   camera,
